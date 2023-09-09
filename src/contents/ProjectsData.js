199cd749429c4beb1fbba7c94c14c6527c73b4c0
Lincoln Li <vivalkm@gmail.com>
2023-09-09 16:05:50 -0700

    Add more projects

--- src/contents/ProjectsData.js
+++ src/contents/ProjectsData.js
@@ -3,7 +3,7 @@ export const projects = [
         title: "Stork",
         imgUrl: "https://user-images.githubusercontent.com/83200994/266770847-3348e6ab-73eb-433e-91a8-d8ef2c8bdf35.png",
         description:
-            "A serverless single-page web application for yard sales, allowing users to easily list and edit items, browse available items, and contact sellers, streamlining the process of organizing and participating in yard sales",
+            "A serverless single-page web application for yard sales, allowing users to easily list and edit items, browse available items, and contact sellers, streamlining the process of organizing and participating in yard sales.",
         demoLink: "https://stork-xi.vercel.app/",
         gitHubLink: "https://github.com/vivalkm/stork",
         skills: ["React.js", "Tailwind CSS", "Firebase", "AWS S3"],
@@ -11,16 +11,17 @@ export const projects = [
     {
         title: "lincoln-li.com",
         imgUrl: "https://github.com/vivalkm/portfolio/assets/83200994/117023eb-f059-4785-a518-8c027bcb2551",
-        description: "A portfolio site built for myself",
+        description:
+            "A personal portfolio site built for myself, implementing various frontend tricks and techniques based on what I learned from CodePen Spark.",
         demoLink: "https://lincoln-li.com",
         gitHubLink: "https://github.com/vivalkm/portfolio",
-        skills: ["React.js", "Tailwind CSS"],
+        skills: ["React.js", "Tailwind CSS", "TweenJS"],
     },
     {
         title: "KidsFun",
         imgUrl: "https://github.com/vivalkm/KidsFun/assets/83200994/f63e71bb-2ca8-47b8-a613-a75efab95715",
         description:
-            "A web application using Express and Node.js to help parents search and share kid-friendly outdoor activities",
+            "A web application using Express and Node.js to help parents search and share kid-friendly outdoor activities. This is my first full stack web application after learning Javascript, HTML, and CSS.",
         demoLink: "https://kidsfun.cyclic.app/",
         gitHubLink: "https://github.com/vivalkm/KidsFun",
         skills: ["Express", "Node.js", "HTML", "CSS", "Bootstrap", "MongoDB", "Cloudinary"],
@@ -29,7 +30,7 @@ export const projects = [
         title: "AWS Helper Bot",
         imgUrl: "https://drive.google.com/uc?id=1IHeVx2VgWd5FPrB4Zj-BsnsxGcUcCFcP",
         description:
-            "A chatbot to manage AWS resources via text and voice commands, simplifying complex AWS configurations across diverse resource consoles",
+            "A chatbot to manage AWS resources via text and voice communications, simplifying complex AWS configurations process, and enhancing user engagement and interaction via Natural Language Understanding feature enabled by Amazon Lex.",
         demoLink: "https://www.youtube.com/watch?v=WqAO6BeNwrA",
         skills: [
             "AWS Lambda",
@@ -39,5 +40,37 @@ export const projects = [
             "AWS Backup",
             "Amazon CloudFront",
         ],
+    },
+    {
+        title: "Bookshelf",
+        imgUrl: "https://github.com/vivalkm/bookshelf/raw/master/walkthrough.gif",
+        description:
+            "A fun project to help me track the books I plan to or have read, showing a dynamic summary of reading statistics. Utilized JSON server for quick backend setup and API testing.",
+        gitHubLink: "https://github.com/vivalkm/bookshelf",
+        skills: ["React.js", "Bulma CSS", "JSON server"],
+    },
+    {
+        title: "BitFit",
+        imgUrl: "https://github.com/vivalkm/BitFit/raw/master/walkthrough.gif",
+        description:
+            "A health metrics android app that allows users to track nutrition calories and mark favorability. Implemented a dashboard fragment where users can see a summary of their entered data.",
+        gitHubLink: "https://github.com/vivalkm/BitFit",
+        skills: ["Kotlin", "Android Studio"],
+    },
+    {
+        title: "WordGuess",
+        imgUrl: "https://github.com/vivalkm/WordGuess/raw/master/walkthrough.gif",
+        description:
+            "An android app that recreates a simple version of the popular word game Wordle. Users try to guess words in three rounds based on hints to unveil a beautiful confetti. I was interesting in learning Kotlin and Android Studio and it spun into this weekend project.",
+        gitHubLink: "https://github.com/vivalkm/WordGuess",
+        skills: ["Kotlin", "Android Studio"],
+    },
+    {
+        title: "Lion King Game",
+        imgUrl: "https://github.com/vivalkm/LionKingGame/raw/master/walkthrough.gif",
+        description:
+            "In summer 2019, while learning Java through the renowned course CS61B at UC Berkeley, I developed a 2D tile-based game that allows players to explore and interact with a pseudorandomly generated world. Since then, my development skills have immensely advanced.",
+        gitHubLink: "https://github.com/vivalkm/LionKingGame",
+        skills: ["Java", "JUnit"],
     },
 ];
