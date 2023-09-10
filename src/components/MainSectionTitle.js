426e53d028e0fda8865ad8aceb6bbde9b08d4ad7
Lincoln Li <vivalkm@gmail.com>
2023-09-09 23:48:14 -0700

    Adjust behavior and shadow for section title

--- src/components/MainSectionTitle.js
+++ src/components/MainSectionTitle.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MainSectionTitle({ children }) {
     return (
-        <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-800/75 px-6 backdrop-blur sm:-mx-6 sm:px-6 md:px-12 sm:sr-only md:relative md:top-auto lg:mx-auto sm:w-full lg:px-0 lg:py-0">
-            <h2 className="text-sm py-3 font-bold uppercase tracking-widest text-slate-200 sm:sr-only">
+        <div className="sticky top-0 z-20 -mx-6  px-6 sm:mx-auto mb-4 w-screen bg-slate-800/75 sm:bg-slate-800/90 sm:shadow-[-5px_5px_15px_5px_rgb(30,41,59)] backdrop-blur sm:px-9 md:px-9 lg:px-0 lg:py-0">
+            <h2 className="text-sm w-full py-3 sm:py-5 font-bold uppercase tracking-widest text-slate-200">
                 {children}
             </h2>
         </div>
