78fdd34be50d7f3f2c0d6280811600f9bd4d8f6a
Lincoln Li <vivalkm@gmail.com>
2023-09-09 19:11:05 -0700

    Add resume

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -6,5 +6,18 @@ export default function Experience() {
     const renderedJobs = jobs.map((job, index) => {
         return <TimeCard key={index}>{job}</TimeCard>;
     });
-    return <div>{renderedJobs}</div>;
+    return (
+        <div>
+            {renderedJobs}
+            <div className="w-full -mt-4 mb-10">
+                <a
+                    href="/Lincoln Li - tech.pdf"
+                    className="float-right text-teal-400 hover:text-teal-200 hover:font-bold"
+                    download
+                >
+                    View Full Resume in PDF
+                </a>
+            </div>
+        </div>
+    );
 }
