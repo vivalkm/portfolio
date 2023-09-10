c9ff21d0afada8500f853e725c28c3b5e2978817
Lincoln Li <vivalkm@gmail.com>
2023-09-09 23:52:19 -0700

    Adjust style for section title

--- src/components/MainSectionTitle.js
+++ src/components/MainSectionTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MainSectionTitle({ children }) {
     return (
-        <div className="sticky top-0 z-20 -mx-6  px-6 sm:mx-auto mb-4 w-screen bg-slate-800/75 sm:bg-slate-800/90 sm:shadow-[-5px_5px_15px_5px_rgb(30,41,59)] backdrop-blur sm:px-9 md:px-9 lg:px-0 lg:py-0">
+        <div className="sticky top-0 z-20 -mx-6 px-6 sm:mx-auto mb-4 w-screen bg-slate-800/75 sm:bg-slate-800/90 sm:shadow-[-5px_5px_15px_5px_rgb(30,41,59)] backdrop-blur sm:px-3">
             <h2 className="text-sm w-full py-3 sm:py-5 font-bold uppercase tracking-widest text-slate-200">
                 {children}
             </h2>
