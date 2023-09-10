2c7d75eec51dd10c79207d260a7ec82180264cb0
Lincoln Li <vivalkm@gmail.com>
2023-09-09 20:44:25 -0700

    Add resume button style

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { jobs } from "../contents/ExperienceData";
 import TimeCard from "../components/TimeCard";
+import { useNavigate } from "react-router-dom";
 
 export default function Experience() {
     const renderedJobs = jobs.map((job, index) => {
         return <TimeCard key={index}>{job}</TimeCard>;
     });
+    const navigate = useNavigate();
     return (
-        <div>
+        <div className="mb-10">
             {renderedJobs}
-            <div className="w-full -mt-4 mb-10">
+            <div className="-mt-4 pb-10">
                 <a
-                    href="/Lincoln Li - tech.pdf"
-                    className="float-right text-teal-400 hover:text-teal-200 hover:font-bold"
-                    download
+                    href="/resume.pdf"
+                    className="float-right text-teal-400 hover:text-teal-200 hover:font-bold border border-teal-200 p-2 rounded hover:shadow-md hover:shadow-teal-100"
+                    target="_blank"
+                    onClick={() => {
+                        navigate("/#experience");
+                    }}
                 >
                     View Full Resume in PDF
                 </a>
